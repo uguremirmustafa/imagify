0d8ef9f302f6f09d6054ce14bbea64d8f80b9d99
uguremirmustafa <uguremirmustafa@gmail.com>
2021-03-23 17:26:18 +0300

    backup

---
+++ hooks/getImages.js
@@ -0,0 +1,5 @@
+import { useQuery } from 'react-query';
+const url = `https://api.unsplash.com/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`;
+
+NEXT_PUBLIC_UNSPLASH_API_KEY = T342GsuPEPPl9AHErdibFyIj6JuxZ - CqFCaZyYibEuo;
+NEXT_PUBLIC_UNSPLASH_SECRET_KEY = -IuSSJj7pLc_0n6CJQzfWlQl9sgRYyf5WvrIY86VtVE;
